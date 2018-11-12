60ff0d0b3fa8faf54bb7cc759610e4a53657191a
Eleazar Araujo <eleazar.araujo@wetransform.to>
2018-11-12 13:20:04 +0100

    add initial project configuration

---
+++ src/app/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <div>
+      Library app!
+    </div>
+  `
+})
+export class HomeComponent {}
