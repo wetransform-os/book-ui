ce7ee29c5dcd20bf9c1d0545c37244704859c985
Eleazar Araujo <eleazar.araujo@wetransform.to>
2018-11-12 15:31:20 +0100

    add LibraryListComponent

---
+++ src/app/library-dashboard/components/library-list/library-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'library-list',
+  templateUrl: './library-list.component.html',
+  styleUrls: ['./library-list.component.scss']
+})
+export class LibraryListComponent {}
