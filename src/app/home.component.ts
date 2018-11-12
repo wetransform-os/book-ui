ce2a5f1c053af1bdf353f996a936780121cc4af1
Eleazar Araujo <eleazar.araujo@wetransform.to>
2018-11-12 17:04:47 +0100

    update styling

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <div class="col-12">
+    <div>
       <span>
         Go to the 
         <a routerLink="/library" class="menu-item">Library</a>
--- src/app/not-found.component.ts
+++ src/app/not-found.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'not-found',
   template: `
-    <div class="col-12">
-      Not found
+    <div>
+      Invalid path
     </div>
   `
 })
